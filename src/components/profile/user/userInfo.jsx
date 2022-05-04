57b78bd5bdc0d1d738ae9f613600457769173000
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-04 11:04:33 +0200

    crear componente user info para datos de imagen y nombre

---
+++ src/components/profile/user/userInfo.jsx
@@ -0,0 +1,13 @@
+import { BsFillPencilFill } from 'react-icons/bs'
+
+export default function userInfo () {
+  return (
+    <>
+      <div>
+        <img src='' alt='' />
+        <BsFillPencilFill />
+      </div>
+      <h2>User Name</h2>
+    </>
+  )
+}

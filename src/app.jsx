b7448a79ee10d399c83de2a0fb433910f9716aa6
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-04 11:00:27 +0200

    exportacion del linknav y su funcion a un componente

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,26 @@
 import './app.css'
 import { BsHouseFill } from 'react-icons/bs'
 import { FaUserAlt } from 'react-icons/fa'
-import { Link, Outlet } from 'react-router-dom'
-import { $ } from './utils/dom'
-import classNames from 'classnames'
-
-function activar (e) {
-  const element = e.target.tagName === 'path' ? e.target.parentNode.parentNode : e.target.parentNode
-  $('.active').classList.toggle('active')
-  element.classList.toggle('active')
-}
-
-function LinkNav (props) {
-  const linkClass = classNames('nav-logo', props.class)
-
-  return (
-    <Link to={props.url} className={linkClass}>
-      {props.icon}
-    </Link>
-  )
-}
+import { Outlet } from 'react-router-dom'
+import { LinkNav } from './components/linkNav'
 
 export default function header () {
   return (
-    <div>
+    <>
       <div className='App-Header'>
         <div className='logo'>
           <img src='/src/assets/therminer icon.png' />
           <h1>Therminer</h1>
         </div>
         <div className='App-Nav'>
-          <LinkNav class='active' url='/' icon={<BsHouseFill onClick={activar} />} />
-          <LinkNav class='' url='/user' icon={<FaUserAlt onClick={activar} />} />
+          <LinkNav id='home-link' name='home' class='active' url='/' icon={<BsHouseFill />} />
+          <LinkNav id='user-link' name='user' class='' url='/profile' icon={<FaUserAlt />} />
           <div className='underline' />
         </div>
       </div>
       <div className='App-Body'>
         <Outlet />
       </div>
-    </div>
+    </>
   )
 }

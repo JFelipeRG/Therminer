dc4cd2d2545b534869033c0429c82aea5faee07f
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-04 17:08:46 +0200

    cambiar a lazy load la page home

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './app.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
 
-const Profile = React.lazy(() => import('./pages/profile'))
+const Home = React.lazy(() => import('./pages/Home/home'))
+const Profile = React.lazy(() => import('./pages/Profile/profile'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<React.Suspense fallback={<>...</>}><Home /></React.Suspense>} />
           <Route path='profile' element={<React.Suspense fallback={<>...</>}><Profile /></React.Suspense>} />
         </Route>
       </Routes>

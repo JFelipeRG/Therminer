d182c78fb633444b2f7b84a1a821e1f09a22bcb6
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-04 11:01:59 +0200

    crear componente wallet

---
+++ src/components/profile/wallet.jsx
@@ -0,0 +1,14 @@
+import { FaWallet } from 'react-icons/fa'
+
+export default function wallet () {
+  return (
+    <div>
+      <FaWallet />
+      <div>
+        <h2>Ganancias totales</h2>
+        <h3>99999 BTC</h3>
+      </div>
+      <h3>+9999 BTC hoy</h3>
+    </div>
+  )
+}

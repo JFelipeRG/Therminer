03ce4703bf472bc4a23518c4035d3211932f0959
Felipe Ramirez <juanfel2024@gmail.com>
2022-05-05 11:17:23 +0200

    crear styled components de user

--- src/components/profile/user/user.style.js
+++ src/components/profile/user/user.style.js
@@ -4,56 +4,49 @@ import { BsFillPencilFill } from 'react-icons/bs'
 export const Img = styled.img`
     margin: auto;
     border-radius: 50%;
-    width: 90px;
-    height: 90px;
+    width: 120px;
+    height: 120px;
 `
 
 export const PencilIcon = styled(BsFillPencilFill)`
     position: absolute;
     top: 0;
-    right: 0;
+    width: 20px;
+    height: 20px;
+    transform: translateX(70px);
     color: grey;
     cursor: pointer;
-`
-
-export const H2 = styled.h2`
-    text-align: center;
-`
-
-export const Div = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-`
-export const DivColumn = styled(Div)`
-    flex-direction: column;
 `
 
 export const Label = styled.label`
     color: grey;
     text-align: left;
     width: 100%;
+    font-size: 18px;
+    margin-top: 15px;
 `
 
 export const Input = styled.input`
     border: none;
     border-bottom: 1px solid grey;
-    &::placeholder {
-        content: 'Hola';
-    }
+    height: 35px;
+    font-size: 16px;
+    width: 98%;
     outline: none;
 `
 
 export const Button = styled.button`
     border: none;
-    width: 60px;
+    width: 80px;
+    height: 35px;
+    font-size: 16px;
+    font-weight: bold;
     background-color: #4c7a34;
     color: white;
     text-align: center;
     padding: 7px 0;
     border-radius: 7px;
-    margin: 5px auto;
+    margin: 20px auto;
     cursor: pointer;
 
     &:hover {
